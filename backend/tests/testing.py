7c134ce6f83dc482583db38506a1ba9017c9d922
Hui-Chong <huichong.me@gmail.com>
2021-09-07 17:11:30 +0800

    TST update the test cases

--- backend/tests/testing.py
+++ backend/tests/testing.py
@@ -1,8 +1,7 @@
+import pytest
 from fastapi.testclient import TestClient
 from ..src.main import app
-from .utils import test_read_by_filter
-from .inputs import *
-
+import pandas as pd
 
 client = TestClient(app)
 
@@ -12,76 +11,15 @@ def test_read_main():
     assert response.status_code == 200
 
 
-def test_read_amps_by_filters():
-    filter_by_options = ['family', 'host', 'sample', 'habitat', 'origin']
-    filter_by_categories = [
-        family_accessions.union(unexpected_inputs),
-        hosts.union(unexpected_inputs),
-        samples.union(unexpected_inputs),
-        habitats.union(unexpected_inputs),
-        origins.union(unexpected_inputs)
-    ]
-    for option, categories in zip(filter_by_options, filter_by_categories):
-        test_read_by_filter(client, query_type='amp', filter_by=option, categories=categories)
+inputs = pd.read_table('tests/inputs.tsv', sep='\t').fillna('')
+inputs['Test path'] = inputs.apply(
+    lambda x: x['API path'].format(accession=x['Input accession']) +
+              ('?{}'.format(x['Input query']) if x['Input query'] != '' else ''),
+    axis=1)
+print(inputs[['Test path', 'Response code']].values.tolist())
 
 
-def test_read_amp():
-    url_prefix = '/v1/amps/'
-    base_urls = {'', '/features', '/distributions', '/metadata'}
-    input_accs = AMP_accessions.union(unexpected_inputs)
-    for base_url in base_urls:
-        url = url_prefix + '{}' + base_url
-        for acc in input_accs:
-            response = client.get(url.format(acc))
-            assert response.status_code == 200
-            if acc in unexpected_inputs:
-                assert response.json() == {}
-            else:
-                assert response.json()['accession'] == acc
-
-
-def test_read_family_by_filters():
-    filter_by_options = ['host', 'sample', 'habitat', 'origin']
-    filter_by_categories = [
-        hosts.union(unexpected_inputs),
-        samples.union(unexpected_inputs),
-        habitats.union(unexpected_inputs),
-        origins.union(unexpected_inputs)
-    ]
-    for option, categories in zip(filter_by_options, filter_by_categories):
-        test_read_by_filter(client, query_type='family', filter_by=option, categories=categories)
-
-
-def test_read_family():
-    url_prefix = '/v1/families/'
-    base_urls = {'', '/features', '/distributions', '/downloads'}
-    input_accs = family_accessions.union(unexpected_inputs)
-    for base_url in base_urls:
-        url = url_prefix + '{}' + base_url
-        for acc in input_accs:
-            response = client.get(url.format(acc))
-            assert response.status_code == 200
-            if acc in unexpected_inputs:
-                assert response.json() == {}
-            else:
-                assert response.json()['accession'] == acc
-
-
-def test_sequence_search():
-    input_sequences = sequences.union(unexpected_inputs)
-    url_base = '/v1/search/{}?query={}'
-    methods = {'mmseqs', 'hmmer'}
-    for method in methods:
-        for seq in input_sequences:
-            print(f'testing search method `{method}` with input `{seq}`')
-            url = url_base.format(method, seq)
-            response = client.get(url)
-            assert response.status_code == 200
-            assertion_result = dict(status_code_checking=response.status_code == 200, data_checking=None)
-            if seq not in sequences:
-                assert response.json() == []
-                assertion_result['data_checking'] = response.json() == []
-            else:
-                assert type(response.json()) == list
-                assertion_result['data_checking'] = type(response.json()) == list
-            print(assertion_result)
+@pytest.mark.parametrize('test_path, response_code', inputs[['Test path', 'Response code']].values.tolist())
+def test_all(test_path, response_code):
+    response = client.get(test_path)
+    assert response.status_code == response_code
