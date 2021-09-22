045ff417e4745684e1482319bdff7cfc2705e040
Hui-Chong <huichong.me@gmail.com>
2021-09-22 22:02:03 +0800

    DEV remove upload button

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -78,33 +78,33 @@
               </el-input>
             </div>
             <br/>
-            <div style="text-align: left">
-              Or upload a file (amino acid sequences, size &lt; 50kb)
-              <br/><br/>
-              <el-upload
-                  class="upload-demo"
-                  drag
-                  action="https://jsonplaceholder.typicode.com/posts/"
-                  :on-preview="handlePreview"
-                  :on-remove="handleRemove"
-                  :file-list="fileList"
-                  multiple
-              >
-                <i class="el-icon-upload"></i>
-                <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-              </el-upload>
+<!--            <div style="text-align: left">-->
+<!--              Or upload a file (amino acid sequences, size &lt; 50kb)-->
+<!--              <br/><br/>-->
 <!--              <el-upload-->
-<!--                action="https://jsonplaceholder.typicode.com/posts/"-->
-<!--                :limit="1"-->
-<!--                :file-list="fastaFile"-->
-<!--                style="text-align: left"-->
-<!--            >-->
-<!--              <el-button size="small" type="primary">Upload a fasta file</el-button>-->
-<!--&lt;!&ndash;              <template #tip>&ndash;&gt;-->
-<!--&lt;!&ndash;                <div class="el-upload__tip"></div>&ndash;&gt;-->
-<!--&lt;!&ndash;              </template>&ndash;&gt;-->
-<!--            </el-upload>-->
-            </div>
+<!--                  class="upload-demo"-->
+<!--                  drag-->
+<!--                  action="https://jsonplaceholder.typicode.com/posts/"-->
+<!--                  :on-preview="handlePreview"-->
+<!--                  :on-remove="handleRemove"-->
+<!--                  :file-list="fileList"-->
+<!--                  multiple-->
+<!--              >-->
+<!--                <i class="el-icon-upload"></i>-->
+<!--                <div class="el-upload__text">Drop file here or <em>click to upload</em></div>-->
+<!--              </el-upload>-->
+<!--&lt;!&ndash;              <el-upload&ndash;&gt;-->
+<!--&lt;!&ndash;                action="https://jsonplaceholder.typicode.com/posts/"&ndash;&gt;-->
+<!--&lt;!&ndash;                :limit="1"&ndash;&gt;-->
+<!--&lt;!&ndash;                :file-list="fastaFile"&ndash;&gt;-->
+<!--&lt;!&ndash;                style="text-align: left"&ndash;&gt;-->
+<!--&lt;!&ndash;            >&ndash;&gt;-->
+<!--&lt;!&ndash;              <el-button size="small" type="primary">Upload a fasta file</el-button>&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;              <template #tip>&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;                <div class="el-upload__tip"></div>&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;&lt;!&ndash;              </template>&ndash;&gt;&ndash;&gt;-->
+<!--&lt;!&ndash;            </el-upload>&ndash;&gt;-->
+<!--            </div>-->
           </el-row>
           <el-row>
             <!--                TODO FIXME-->
