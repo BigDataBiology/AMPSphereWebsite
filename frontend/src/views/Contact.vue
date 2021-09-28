625b13fed007176b06bd22e4cde1a0487c004cd6
Hui-Chong <huichong.me@gmail.com>
2021-09-28 17:29:18 +0800

    DEV update contact page

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -8,23 +8,27 @@
         </el-breadcrumb>
 
         <h1>Contact</h1>
-        <p>AMPsphere also is discussed in a forum where users can suggest improvements, issues, ongoing projects, etc: <a href="https://groups.google.com/g/ampsphere-users">Go to our community now!</a></p>
-        <p>This is a project hosted by the <a href="big-data-biology.org/">Big Data Biology Research Group</a>, visit us at this link!</p>
+        <p>AMPsphere also is discussed in a forum where users can suggest improvements, issues, ongoing projects, etc: <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">Go to our community now!</el-link></p>
+        <p>This is a project hosted by the <el-link type='primary' href="big-data-biology.org/">Big Data Biology Research Group</el-link>, visit us at this link!</p>
         <hr />
 
 <!--     TODO two columns: names go to the left side, and affiliation goes to the right side.-->
 
         <h3 id="contact-us-if-you-have-issues-or-suggestions">Contact us if you have issues or suggestions:</h3>
-        <p>To direct communication:</p>
-        <ul>
-          <li><a href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</a></li>
-          <li><a href="mailto:hui@big-data-biology.org">Hui Chong</a></li>
-          <li><a href="mailto:luis@luis.org">Luis Pedro Coelho</a></li>
-        </ul>
-        <p><strong>Affiliation:</strong></p>
-        <p>Institute of Science and Technology for Brain-Inspired Intelligence (<a href="istbi.fudan.edu.cn/">ISTBI</a>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
-
-
+        <el-row>
+          <el-col :span="11">
+            <p><strong>To direct communication:</strong></p>
+            <ul>
+              <li><el-link type='primary' href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</el-link></li>
+              <li><el-link type='primary' href="mailto:hui@big-data-biology.org">Hui Chong</el-link></li>
+              <li><el-link type='primary' href="mailto:luis@luis.org">Luis Pedro Coelho</el-link></li>
+            </ul>
+          </el-col>
+          <el-col :span="11" :offset="1">
+            <p><strong>Affiliation:</strong></p>
+            <p>Institute of Science and Technology for Brain-Inspired Intelligence (<el-link type='primary' href="istbi.fudan.edu.cn/">ISTBI</el-link>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
