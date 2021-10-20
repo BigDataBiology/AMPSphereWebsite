9bfed251746edee6c435dfac56f328bf3ad70273
Hui-Chong <huichong.me@gmail.com>
2021-10-20 14:12:19 +0800

    ENH include full AMPSphere database and search databases on the Downloads page

--- frontend/src/views/Downloads.vue
+++ frontend/src/views/Downloads.vue
@@ -10,7 +10,8 @@
         <div class="subsection-title">Core resources</div>.
         <el-table :data="downloadable_files" width="100%">
           <el-table-column prop="name" label="Name" width="200%"></el-table-column>
-          <el-table-column label="URL" width="200%">
+          <el-table-column prop="type" label="Type" width="150%"></el-table-column>
+          <el-table-column label="URL" width="150%">
             <template #default="props">
               <el-link :href="props.row.file" type="primary">Download</el-link>
             </template>
@@ -38,21 +39,46 @@ export default {
     return {
       downloadable_files: [
         {
+          name: 'AMPSphere database',
+          type: 'Full database',
+          file: "http://18.140.248.253:443/v1/downloads/main_db",
+          desc: "Full sqlite3 database containing all individual tables below.",
+        },
+        {
           name: "AMP",
-          file: "http://18.140.248.253:8080/AMP.tsv",
-          desc: "All AMP families and sequences."},
+          type: "Individual table",
+          file: "http://18.140.248.253:443/v1/downloads/tables:AMP",
+          desc: "All AMP families and sequences."
+        },
         {
           name: "GMSC",
-          file: "http://18.140.248.253:8080/GMSC.tsv",
-          desc: "All AMP-associated smORF genes and sequences."},
+          type: "Individual table",
+          file: "http://18.140.248.253:443/v1/downloads/tables:GMSC",
+          desc: "All AMP-associated smORF genes and sequences."
+        },
         {
           name: "Metadata",
-          file: "http://18.140.248.253:8080/Metadata.tsv",
+          type: "Individual table",
+          file: "http://18.140.248.253:443/v1/downloads/tables:Metadata",
           desc: "Host and taxonomic origins, associated habitats, metagenome samples and genomes."},
         {
           name: "Statistics",
-          file: "http://18.140.248.253:8080/Statistics.tsv",
-          desc: "Basic statistics of the AMPSphere database."}
+          type: "Individual table",
+          file: "http://18.140.248.253:443/v1/downloads/tables:Statistics",
+          desc: "Basic statistics of the AMPSphere database."
+        },
+        {
+          name: "MMseqs database",
+          type: "Search database",
+          file: "http://18.140.248.253:443/v1/downloads/search_db:mmseqs",
+          desc: 'Search database for offline or large scale query, can be directly used by the MMseqs software.'
+        },
+        {
+          name: "HMM profile database",
+          type: "Search database",
+          file: "http://18.140.248.253:443/v1/downloads/search_db:hmmer",
+          desc: 'Search database for offline or large scale query, can be directly used by the HMMER software.'
+        }
       ]
     }
   }
