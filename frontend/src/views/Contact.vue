a3356d85d28c6294aac6c261acebd73b0a89f7c4
Hui-Chong <huichong.me@gmail.com>
2021-11-17 23:14:47 +0800

    ENH better layout

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="Contact">
     <div class="row justify-center">
-      <div class="col-xs-0 col-xl-2 bg-white"></div>
-      <div class="col-12 col-xl-8 justify-center q-pr-md q-ma-auto">
-        <h4>Contact</h4>
+      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg">
         <p>AMPsphere also is discussed in a forum where users can suggest improvements, issues, ongoing projects, etc: <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">Go to our community now!</el-link></p>
         <p>This is a project hosted by the <el-link type='primary' href="big-data-biology.org/">Big Data Biology Research Group</el-link>, visit us at this link!</p>
-        <hr />
-        <h5 id="contact-us-if-you-have-issues-or-suggestions">Contact us if you have issues or suggestions:</h5>
-        <div class="row">
-          <div class="col-12 col-lg-6">
-            <p><strong>To direct communication:</strong></p>
-            <ul>
-              <li><el-link type='primary' href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</el-link></li>
-              <li><el-link type='primary' href="mailto:hui@big-data-biology.org">Hui Chong</el-link></li>
-              <li><el-link type='primary' href="mailto:luis@luis.org">Luis Pedro Coelho</el-link></li>
-            </ul>
-          </div>
-          <div class="col-12 col-lg-6">
-            <p><strong>Affiliation:</strong></p>
-            <p>Institute of Science and Technology for Brain-Inspired Intelligence (<el-link type='primary' href="istbi.fudan.edu.cn/">ISTBI</el-link>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
-          </div>
-        </div>
       </div>
-      <div class="col-xs-0 col-xl-2 bg-white"></div>
+      <div class="col-lg-2"></div>
+      <div class="text-h5 col-12 col-lg-8 offset-lg-2 text-center q-px-lg text-bold">
+          Contact us if you have issues or suggestions:
+      </div>
+      <div class="col-lg-2"></div>
+      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg ">
+        <p class="text-h6"><strong>To direct communication:</strong></p>
+        <ul>
+          <li><el-link type='primary' href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</el-link></li>
+          <li><el-link type='primary' href="mailto:hui@big-data-biology.org">Hui Chong</el-link></li>
+          <li><el-link type='primary' href="mailto:luis@luis.org">Luis Pedro Coelho</el-link></li>
+        </ul>
+      </div>
+      <div class="col-lg-2"></div>
+      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg ">
+        <p class="text-h6"><strong>Affiliation:</strong></p>
+        <p>Institute of Science and Technology for Brain-Inspired Intelligence (<el-link type='primary' href="istbi.fudan.edu.cn/">ISTBI</el-link>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
+      </div>
+      <div class="col-lg-2"></div>
     </div>
+  </div>
 <!--    <el-row>-->
 <!--      <el-col :span="22" :offset="1">-->
 <!--        <el-breadcrumb separator-class="el-icon-arrow-right">-->
@@ -56,7 +57,6 @@
 <!--        </el-row>-->
 <!--      </el-col>-->
 <!--    </el-row>-->
-  </div>
 </template>
 
 <script>
