4178a8b8dcfef3777dbee3f68517c73b5c6688ac
Luis Pedro Coelho <luis@luispedro.org>
2021-11-23 11:38:48 +0100

    MIN Slightly better contact page
    - Better text
    - Align left

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="Contact">
     <div class="row justify-center">
-      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg">
-        <p>AMPsphere also is discussed in a forum where users can suggest improvements, issues, ongoing projects, etc: <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">Go to our community now!</el-link></p>
-        <p>This is a project hosted by the <el-link type='primary' href="big-data-biology.org/">Big Data Biology Research Group</el-link>, visit us at this link!</p>
+      <div class="col-12 col-lg-8 offset-lg-2 text-left q-px-lg">
+        <p>AMPsphere is discussed in <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">a forum</el-link> where users can suggest improvements, issues, ongoing projects, etc: <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">https://groups.google.com/g/ampsphere-users</el-link>.</p>
+        <p>This is a project hosted by the <el-link type='primary' href="https://big-data-biology.org/">Big Data Biology Research Group</el-link>, visit us at this link!</p>
       </div>
       <div class="col-lg-2"></div>
-      <div class="text-h5 col-12 col-lg-8 offset-lg-2 text-center q-px-lg text-bold">
+      <div class="text-h5 col-12 col-lg-8 offset-lg-2 q-px-lg text-bold">
           Contact us if you have issues or suggestions:
       </div>
       <div class="col-lg-2"></div>
-      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg ">
+      <div class="col-12 col-lg-8 offset-lg-2 q-px-lg ">
         <p class="text-h6"><strong>To direct communication:</strong></p>
         <ul>
           <li><el-link type='primary' href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</el-link></li>
           <li><el-link type='primary' href="mailto:hui@big-data-biology.org">Hui Chong</el-link></li>
-          <li><el-link type='primary' href="mailto:luis@luis.org">Luis Pedro Coelho</el-link></li>
+          <li><el-link type='primary' href="mailto:luispedro@big-data-biology.org">Luis Pedro Coelho</el-link></li>
         </ul>
       </div>
       <div class="col-lg-2"></div>
