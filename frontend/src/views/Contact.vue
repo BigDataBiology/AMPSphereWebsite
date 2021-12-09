12055451f3dbd810685dd16b62aa5149c6aab3e4
Hui-Chong <huichong.me@gmail.com>
2021-12-09 17:12:50 +0800

    ENH better contact page

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,30 +1,43 @@
 <template>
   <div id="Contact">
     <div class="row justify-center">
-      <div class="col-12 col-lg-8 offset-lg-2 text-left q-px-lg">
-        <p>AMPsphere is discussed in <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">a forum</el-link> where users can suggest improvements, issues, ongoing projects, etc: <el-link type='primary' href="https://groups.google.com/g/ampsphere-users">https://groups.google.com/g/ampsphere-users</el-link>.</p>
-        <p>This is a project hosted by the <el-link type='primary' href="https://big-data-biology.org/">Big Data Biology Research Group</el-link>, visit us at this link!</p>
+      <div class="col-xs-0 col-xl-2 bg-white"></div>
+      <div class="col-12 col-xl-8 justify-center q-px-md q-ma-auto">
+        <div class="row justify-center">
+          <div class="col-12 text-center">
+            <p>AMPsphere is discussed in <a href="https://groups.google.com/g/ampsphere-users">a forum</a> where users can suggest improvements, issues, ongoing projects, etc: <a href="https://groups.google.com/g/ampsphere-users">https://groups.google.com/g/ampsphere-users</a>.</p>
+            <p>This is a project hosted by the <a href="https://big-data-biology.org/">Big Data Biology Research Group</a>, visit us at this link!</p>
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="text-h5 col-12 text-bold text-center q-py-md">
+            Contact us if you have issues or suggestions:
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="col-12 col-lg-4 text-center">
+            <p class="text-h6"><strong>To direct communication:</strong></p>
+            <a href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</a><br/>
+            <a href="mailto:hui@big-data-biology.org">Hui Chong</a><br/>
+            <a href="mailto:luispedro@big-data-biology.org">Luis Pedro Coelho</a><br/>
+          </div>
+          <div class="col-12 col-lg-8 text-center ">
+            <p class="text-h6"><strong>Affiliation:</strong></p>
+            <p>Institute of Science and Technology for Brain-Inspired Intelligence (<a href="istbi.fudan.edu.cn/">ISTBI</a>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
+          </div>
+        </div>
+        <div class="row justify-center">
+          <div class="text-h5 col-12 text-bold text-center q-py-md">Acknowledgements</div>
+          <div class="text-center">
+            <p>This project is conducted in collaboration with the <a href="https://www.embl.org/groups/bork/">Bork Group at EMBL</a> and the <a href="http://compgenomics.org/">Huerta-Cepas group at CPGB</a>.</p>
+            <p>We would like to acknowledge in special: T.S.B. Schmidt; A.N. Fullam; Y. Duan; P. Bork; X. Zhao and J. Huerta-Cepas.</p>
+          </div>
+        </div>
+
       </div>
-      <div class="col-lg-2"></div>
-      <div class="text-h5 col-12 col-lg-8 offset-lg-2 q-px-lg text-bold">
-          Contact us if you have issues or suggestions:
-      </div>
-      <div class="col-lg-2"></div>
-      <div class="col-12 col-lg-8 offset-lg-2 q-px-lg ">
-        <p class="text-h6"><strong>To direct communication:</strong></p>
-        <ul>
-          <li><el-link type='primary' href="mailto:celio@big-data-biology.org">Célio Dias Santos Júnior</el-link></li>
-          <li><el-link type='primary' href="mailto:hui@big-data-biology.org">Hui Chong</el-link></li>
-          <li><el-link type='primary' href="mailto:luispedro@big-data-biology.org">Luis Pedro Coelho</el-link></li>
-        </ul>
-      </div>
-      <div class="col-lg-2"></div>
-      <div class="col-12 col-lg-8 offset-lg-2 text-center q-px-lg ">
-        <p class="text-h6"><strong>Affiliation:</strong></p>
-        <p>Institute of Science and Technology for Brain-Inspired Intelligence (<el-link type='primary' href="istbi.fudan.edu.cn/">ISTBI</el-link>). 23rd Floor, East Main Building of Guanghua Tower Fudan University 220 Handan Road, Yangpu District Shanghai(200433), China.</p>
-      </div>
-      <div class="col-lg-2"></div>
+      <div class="col-xs-0 col-xl-2 bg-white"></div>
     </div>
+
   </div>
 <!--    <el-row>-->
 <!--      <el-col :span="22" :offset="1">-->
