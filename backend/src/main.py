e66e731c5ea1155ca7adab47d45a010ae595dfc0
Hui-Chong <huichong.me@gmail.com>
2021-12-13 16:53:37 +0800

    ENH remove FastAPI middleware and use Nginx equivalent

--- backend/src/main.py
+++ backend/src/main.py
@@ -27,18 +27,19 @@ app = FastAPI(
         "name": "MIT",
         "url": "https://github.com/BigDataBiology/AMPSphereWebsite/blob/main/LICENSE",
     },
-    docs_url='/redoc',
-    redoc_url='/',
+    docs_url='/',
+    redoc_url='/redoc',
     openapi_url='/openapi.json'
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
-)
+# Repeated with Nginx one, so remove it.
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+#     allow_credentials=True,
+# )
 
 
 for router in [amp_router, family_router, default_router]:
